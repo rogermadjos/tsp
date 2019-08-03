12c895ed08e2c6783265092c8f815b55f6a4d1c2
Roger Madjos <roger.madjos@gmail.com>
2019-08-03 23:01:13 +0800

    feat: implement crossover

--- src/genetic.rs
+++ src/genetic.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+extern crate speculate;
+
 #[path = "tsp.rs"]
 mod tsp;
 
@@ -10,7 +13,7 @@ use std::cmp::Ordering;
 
 #[derive(Clone)]
 struct Individual {
-  solution: Vec<usize>,
+  chromosome: Vec<usize>,
   fitness: f64,
 }
 
@@ -34,18 +37,39 @@ impl PartialEq for Individual {
 
 impl Eq for Individual {}
 
-fn generate_random_individual(cities: &Vec<City>, rng: &mut ThreadRng) -> Individual {
-  let mut solution: Vec<usize> = (0..cities.len()).collect();
-  solution.shuffle(rng);
-  let fitness = 1. / total_distance(cities, &solution);
+fn random_individual(cities: &Vec<City>, rng: &mut ThreadRng) -> Individual {
+  let mut perm: Vec<usize> = (0..cities.len()).collect();
+  perm.shuffle(rng);
+
+  let fitness = 1. / total_distance(cities, &perm);
 
   Individual {
-    solution,
+    chromosome: invert(&perm),
     fitness,
   }
 }
 
-fn crossover(parent1: &Individual, parent2: &Individual, mark: usize) {
+fn crossover(parents: (&Vec<usize>, &Vec<usize>), points: (usize, usize)) -> (Vec<usize>, Vec<usize>) {
+  let len = parents.0.len();
+
+  let pns = if points.0 <= points.1 {
+    points.1 - points.0
+  } else {
+    parents.0.len() - points.0 + points.1
+  };
+
+  let mut offsprings = (parents.0.clone(), parents.1.clone());
+
+  if pns > 0 {
+    for i in 0..pns {
+      let j = (points.0 + i) % len;
+      offsprings.0[j] = parents.1[j];
+      offsprings.1[j] = parents.0[j];
+    }
+  }
+
+
+  offsprings
 }
 
 fn invert(perm: &Vec<usize>) -> Vec<usize> {
@@ -67,7 +91,7 @@ fn invert(perm: &Vec<usize>) -> Vec<usize> {
   inverse
 }
 
-fn original(inv: &Vec<usize>) -> Vec<usize> {
+fn revert(inv: &Vec<usize>) -> Vec<usize> {
   let mut perm = vec![0; inv.len()];
   let mut pos = vec![0; inv.len()];
   for i in (0..inv.len()).rev() {
@@ -94,7 +118,7 @@ pub fn solve(cities: &Vec<City>) -> Vec<usize> {
   let survivors_size = population_size / 2;
   let mut population = Vec::new();
   for _ in 0..population_size {
-    population.push(generate_random_individual(cities, &mut rng));
+    population.push(random_individual(cities, &mut rng));
   }
 
   population.sort();
@@ -109,25 +133,62 @@ pub fn solve(cities: &Vec<City>) -> Vec<usize> {
 
 #[cfg(test)]
 mod tests {
-
   use super::*;
+  use speculate::speculate;
 
-  const EXAMPLES: [([usize; 7], [usize; 7]); 2] = [
-    ( [5, 1, 2, 3, 0, 6, 4], [4, 1, 1, 1, 2, 0, 0] ),
-    ( [4, 1, 3, 0, 2, 6, 5], [3, 1, 2, 1, 0, 1, 0] )
-  ];
+  speculate! {
+    describe "invert" {
+      const EXAMPLES: [([usize; 7], [usize; 7]); 2] = [
+        ( [5, 1, 2, 3, 0, 6, 4], [4, 1, 1, 1, 2, 0, 0] ),
+        ( [4, 1, 3, 0, 2, 6, 5], [3, 1, 2, 1, 0, 1, 0] )
+      ];
 
-  #[test]
-  fn test_invert() {
-    for (input, output) in EXAMPLES.iter() {
-      assert_eq!(&invert(&input.to_vec()), output);
+      it "should generate correct output" {
+        for (input, output) in EXAMPLES.iter() {
+          assert_eq!(&invert(&input.to_vec()), output);
+        }
+      }
     }
-  }
 
-  #[test]
-  fn test_original() {
-    for (output, input) in EXAMPLES.iter() {
-      assert_eq!(&original(&input.to_vec()), output);
+    describe "revert" {
+      const EXAMPLES: [([usize; 7], [usize; 7]); 2] = [
+        ( [4, 1, 1, 1, 2, 0, 0], [5, 1, 2, 3, 0, 6, 4] ),
+        ( [3, 1, 2, 1, 0, 1, 0], [4, 1, 3, 0, 2, 6, 5] )
+      ];
+
+      it "should generate correct output" {
+        for (input, output) in EXAMPLES.iter() {
+          assert_eq!(&revert(&input.to_vec()), output);
+        }
+      }
+    }
+
+    describe "crossover" {
+      const PARENTS: ([usize; 7], [usize; 7]) = ([4, 1, 1, 1, 2, 0, 0], [3, 1, 2, 1, 0, 1, 0]);
+
+      describe "given point 1 is greater than point 2" {
+        const POINTS: (usize, usize) = (1,4);
+
+        it "should generate correct output" {
+          assert_eq!(crossover((&PARENTS.0.to_vec(), &PARENTS.1.to_vec()), POINTS), ([4, 1, 2, 1, 2, 0, 0].to_vec(), [3, 1, 1, 1, 0, 1, 0].to_vec()));
+        }
+      }
+
+      describe "given point 2 is greater than point 1" {
+        const POINTS: (usize, usize) = (5, 2);
+
+        it "should generate correct output" {
+          assert_eq!(crossover((&PARENTS.0.to_vec(), &PARENTS.1.to_vec()), POINTS), ([3, 1, 1, 1, 2, 1, 0].to_vec(), [4, 1, 2, 1, 0, 0, 0].to_vec()));
+        }
+      }
+
+      describe "given point 1 is equal to point 2" {
+        const POINTS: (usize, usize) = (4, 4);
+
+        it "should generate offsprings equal to the parents" {
+          assert_eq!(crossover((&PARENTS.0.to_vec(), &PARENTS.1.to_vec()), POINTS), ([4, 1, 1, 1, 2, 0, 0].to_vec(), [3, 1, 2, 1, 0, 1, 0].to_vec()));
+        }
+      }
     }
   }
 }
